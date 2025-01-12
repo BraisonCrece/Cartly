5285d5b05a96d4125aa3e4dd8c49228b4c0e9cb7
BraisonCrece <104019638+BraisonCrece@users.noreply.github.com>
2025-01-12 22:54:57 +0100

    feat(navbar, scroll): enhance navbar and scrolling interaction
    Added new styles to hide scrollbars on overflow using CSS and updated JavaScript
    controllers with IntersectionObserver for better linking and scrolling experience
    in the navbar. Refactored code for consistency and efficiency in both JavaScript
    and ERB views, ensuring smooth integration and user-friendly interactions.

--- app/javascript/controllers/scroll_controller.js
+++ app/javascript/controllers/scroll_controller.js
@@ -2,22 +2,70 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="scroll"
 export default class extends Controller {
+  static targets = ["navbar", "categoryHeader"];
+
+  initialize() {
+    // Improve this, if the target has to make the scroll go down, do not go up and then down. Just go down.
+    this.observer = new IntersectionObserver(
+      (entries, _observer) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            const center = Array.from(this.navbarTarget.children).find(
+              (navbarLink) => {
+                return (
+                  navbarLink.getAttribute("href") ===
+                  `#${entry.target.children[0].id}`
+                );
+              },
+            );
+            this.centerElement(center);
+          }
+        });
+      },
+      { root: null, rootMargin: "0px", threshold: 1.0 },
+    );
+  }
+
+  connect() {
+    this.categoryHeaderTargets.forEach((target) => {
+      this.observer.observe(target.parentElement);
+    });
+  }
+
   scrollToCategory(event) {
     event.preventDefault();
     const targetId = event.currentTarget.getAttribute("href");
     const targetElement = document.querySelector(targetId);
 
-    const scrollToTarget = () => {
-      const headerOffset = -90;
-      const elementPosition = targetElement.offsetTop;
-      const offsetPosition = elementPosition - headerOffset;
+    this.scrollToTarget(targetElement);
+  }
 
-      window.scrollTo({
-        top: offsetPosition,
-        behavior: "smooth"
-      });
-    };
+  centerElement(element) {
+    const navbar = this.navbarTarget;
+    const elementOffset = element.offsetLeft;
+    const elementWidth = element.offsetWidth;
+    const navbarWidth = navbar.offsetWidth;
 
-    scrollToTarget();
+    const scrollLeft = elementOffset - navbarWidth / 2 + elementWidth / 2;
+
+    navbar.scrollTo({
+      left: scrollLeft,
+      behavior: "smooth",
+    });
+  }
+
+  scrollToTarget(targetElement) {
+    const headerOffset = -75;
+    const elementPosition = targetElement.offsetTop;
+    const offsetPosition = elementPosition - headerOffset;
+
+    window.scrollTo({
+      top: offsetPosition,
+      behavior: "smooth",
+    });
+  }
+
+  disconnect() {
+    this.observer.disconnect();
   }
 }
